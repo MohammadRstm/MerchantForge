d43c9c121387168ed2059f76580dbd5c83455553
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-08 22:45:55 +0300

    feat:Added global exception handler file

---
+++ MerchForge.api/Exceptions/GlobalExceptionHanlder.cs
@@ -0,0 +1,89 @@
+﻿using MerchForge.api.Exceptions.Auth;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace MerchForge.api.Exceptions
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+
+        public GlobalExceptionHandler(
+            ILogger<GlobalExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public async ValueTask<bool> TryHandleAsync(
+         HttpContext httpContext,
+         Exception exception,
+         CancellationToken cancellationToken)
+        {
+            _logger.LogError(
+                exception,
+                "An unhandled exception occurred.");
+
+            var statusCode = exception switch
+            {
+                EmailAlreadyExistsException => StatusCodes.Status409Conflict,
+
+                InvalidCredentialsException =>
+                    StatusCodes.Status401Unauthorized,
+
+                InvalidRefreshTokenException =>
+                    StatusCodes.Status401Unauthorized,
+
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            var response = new ProblemDetails
+            {
+                Status = statusCode,
+                Title = GetTitle(statusCode),
+                Detail = GetDetail(exception, statusCode),
+                Instance = httpContext.Request.Path
+            };
+
+            httpContext.Response.StatusCode = statusCode;
+
+            await httpContext.Response.WriteAsJsonAsync(
+                response,
+                cancellationToken);
+
+            return true;
+        }
+
+        private static string GetTitle(int statusCode)
+        {
+            return statusCode switch
+            {
+                StatusCodes.Status400BadRequest =>
+                    "Bad Request",
+
+                StatusCodes.Status401Unauthorized =>
+                    "Unauthorized",
+
+                StatusCodes.Status403Forbidden =>
+                    "Forbidden",
+
+                StatusCodes.Status404NotFound =>
+                    "Not Found",
+
+                StatusCodes.Status409Conflict =>
+                    "Conflict",
+
+                _ => "Internal Server Error"
+            };
+        }
+
+        private static string GetDetail(
+            Exception exception,
+            int statusCode)
+        {
+            return statusCode == StatusCodes.Status500InternalServerError
+                ? "An unexpected error occurred."
+                : exception.Message;
+        }
+    }
+}

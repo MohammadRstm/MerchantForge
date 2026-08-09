d887d7531c34c0680644b05acdf1ec95db279235
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-09 16:16:09 +0300

    feat:Implemented login request validator

--- MerchForge.api/Validators/Auth/LoginRequestValidator.cs
+++ MerchForge.api/Validators/Auth/LoginRequestValidator.cs
@@ -1,6 +1,21 @@
-﻿namespace MerchForge.api.Validators.Auth
+﻿using FluentValidation;
+using MerchForge.api.DTOs.Auth;
+
+
+namespace MerchForge.api.Validators.Auth
 {
-    public class LoginRequestValidator
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
     {
+        public LoginRequestValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .MaximumLength(255);
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(8);
+        }
     }
 }

3d91c43c8293c4dfc54877afa99521428eefe403
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-09 18:12:19 +0300

    feat:Implemented static class for authorization policy names

--- MerchForge.api/Authorization/AuthorizationPolicies.cs
+++ MerchForge.api/Authorization/AuthorizationPolicies.cs
@@ -1,6 +1,13 @@
 ﻿namespace MerchForge.api.Authorization
 {
-    public class AuthorizationPolicies
+    public static class AuthorizationPolicies
     {
+        public const string SystemAdmin = "SystemAdmin";
+
+        public const string BusinessMember = "BusinessMember";
+
+        public const string BusinessAdmin = "BusinessAdmin";
+
+        public const string BusinessOwner = "BusinessOwner";
     }
 }

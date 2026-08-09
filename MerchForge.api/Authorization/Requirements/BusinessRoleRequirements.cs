572ed71767586f435ca8c145383d8a4adc0bc421
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-09 17:57:47 +0300

    feat:created business role requirement

---
+++ MerchForge.api/Authorization/Requirements/BusinessRoleRequirements.cs
@@ -0,0 +1,17 @@
+﻿using MerchForge.api.Enums;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace MerchForge.api.Authorization.Requirements
+{
+    public class BusinessRoleRequirements
+    {
+        public IReadOnlyCollection<BusinessRole> AllowedRoles { get; }
+
+        public BusinessRoleRequirements(
+         params BusinessRole[] allowedRoles)
+        {
+            AllowedRoles = allowedRoles;
+        }
+    }
+}

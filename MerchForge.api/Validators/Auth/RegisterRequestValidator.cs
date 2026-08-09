70aa2b532e1e5454a1ffd85b15ef9af8197ea3f3
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-09 16:13:32 +0300

    feat:Implemented register request validator

--- MerchForge.api/Validators/Auth/RegisterRequestValidator.cs
+++ MerchForge.api/Validators/Auth/RegisterRequestValidator.cs
@@ -1,6 +1,33 @@
-﻿namespace MerchForge.api.Validators.Auth
+﻿using FluentValidation;
+using MerchForge.api.DTOs.Auth;
+
+namespace MerchForge.api.Validators.Auth
 {
-    public class RegisterRequestValidator
+    public class RegisterRequestValidator :AbstractValidator<RegisterRequest>
     {
+        public RegisterRequestValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .MaximumLength(50);
+
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .MaximumLength(50);
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .MaximumLength(255);
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(8);
+
+            RuleFor(x => x.BusinessName)
+                .NotEmpty()
+                .MaximumLength(100);
+        }
     }
 }
+

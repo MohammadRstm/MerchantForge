1dc4046dd9853abae8c16065879f6a8e73f440b3
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-13 19:37:54 +0300

    feat:added the user interface repo with initial functions declarations

--- MerchForge.api/Repositories/Interfaces/IUserRepository.cs
+++ MerchForge.api/Repositories/Interfaces/IUserRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace MerchForge.api.Repositories.Interfaces
+﻿using MerchForge.api.Models;
+
+namespace MerchForge.api.Repositories.Interfaces
 {
     public interface IUserRepository
     {
+        Task<User?> GetByEmailAsync(
+          string email,
+          CancellationToken cancellationToken = default);
+
+        Task AddAsync(
+          User user,
+          CancellationToken cancellationToken = default);
     }
 }

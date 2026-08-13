2fe872d70be90f16fb4559a173ee34099fbb6f72
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-13 19:32:22 +0300

    forgot what I did

--- MerchForge.UnitTests/Services/AuthServiceTests.cs
+++ MerchForge.UnitTests/Services/AuthServiceTests.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using MerchForge.api.Factory;
+using MerchForge.api.Models;
+using MerchForge.api.Services.Auth.interfaces;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +11,21 @@ namespace MerchForge.UnitTests.Services
 {
     internal class AuthServiceTests
     {
+        private readonly Mock<IPasswordHasher<User>> _passwordHasherMock;
+        private readonly Mock<IJwtService> _jwtServiceMock;
+        private readonly Mock<IRefreshTokenService> _refreshTokenServiceMock;
+        private readonly Mock<IRegistrationFactory> _registrationFactoryMock;
+
+        public AuthServiceTests()
+        {
+            _passwordHasherMock = new Mock<IPasswordHasher<User>>();
+            _jwtServiceMock = new Mock<IJwtService>();
+            _refreshTokenServiceMock = new Mock<IRefreshTokenService>();
+            _registrationFactoryMock = new Mock<IRegistrationFactory>();
+        }
+
+
+
 
     }
 }

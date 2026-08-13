54e58308dccb488da4c7d85761898ca5037812a5
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-13 19:39:15 +0300

    feat:IMplemented the Iuserrepo class

--- MerchForge.api/Repositories/Implementations/UserRepository.cs
+++ MerchForge.api/Repositories/Implementations/UserRepository.cs
@@ -1,9 +1,34 @@
-﻿using MerchForge.api.Repositories.Interfaces;
+﻿using MerchForge.api.Data;
+using MerchForge.api.Models;
+using MerchForge.api.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace MerchForge.api.Repositories.Implementations
 {
     public class UserRepository : IUserRepository
     {
+        private readonly MerchForgeDbContext _db;
 
+        public UserRepository(MerchForgeDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<User?> GetByEmailAsync(
+           string email,
+           CancellationToken cancellationToken = default)
+        {
+            return await _db.Users
+                .FirstOrDefaultAsync(
+                    u => u.Email == email,
+                    cancellationToken);
+        }
+
+        public async Task AddAsync(
+            User user,
+            CancellationToken cancellationToken = default)
+        {
+            await _db.Users.AddAsync(user, cancellationToken);
+        }
     }
 }

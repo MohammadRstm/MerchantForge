aebedf7cb30f58929b1c6079706497a199ce387f
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-14 14:41:16 +0300

    feat:created the features model

---
+++ MerchForge.api/Models/Features.cs
@@ -0,0 +1,23 @@
+﻿namespace MerchForge.api.Models;
+
+public class Feature
+{
+    public Guid Id { get; set; }
+
+    public string Key { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public bool IsActive { get; set; } = true;
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+    public ICollection<PlanFeature> PlanFeatures { get; set; }
+        = new List<PlanFeature>();
+
+    public ICollection<BusinessFeatureOverride> BusinessOverrides { get; set; }
+        = new List<BusinessFeatureOverride>();
+}

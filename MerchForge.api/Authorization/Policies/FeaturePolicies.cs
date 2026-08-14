af6bbcaaa20037badf49f9f5786df31e7f2cf1c5
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-14 15:20:53 +0300

    feat:Added initial feature policies

---
+++ MerchForge.api/Authorization/Policies/FeaturePolicies.cs
@@ -0,0 +1,19 @@
+﻿namespace MerchForge.api.Authorization.Policies
+{
+    public class FeaturePolicies
+    {
+        public const string Products = "products";
+
+        public const string TelegramProductCreation =
+            "telegram_product_creation";
+
+        public const string WhatsAppProductCreation =
+            "whatsapp_product_creation";
+
+        public const string AiProductGeneration =
+            "ai_product_generation";
+
+        public const string AiImageGeneration =
+            "ai_image_generation";
+    }
+}

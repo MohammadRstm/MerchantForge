0673c03924c313fa4094eab35d96f618747060bc
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-14 14:48:54 +0300

    feat:created the business feature override model for custome plans

---
+++ MerchForge.api/Models/BusinessFeatureOverride.cs
@@ -0,0 +1,22 @@
+﻿namespace MerchForge.api.Models;
+
+public class BusinessFeatureOverride
+{
+    public Guid Id { get; set; }
+
+    public Guid BusinessId { get; set; }
+
+    public Guid FeatureId { get; set; }
+
+    public bool IsEnabled { get; set; }
+
+    public int? Limit { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public Business Business { get; set; } = null!;
+
+    public Feature Feature { get; set; } = null!;
+}

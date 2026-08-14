b61efc00329cab2747697b3ddb00f6ddd13e2ee5
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-14 14:50:12 +0300

    feat:Added create feature request

---
+++ MerchForge.api/DTOs/Subscriptions/CreateFeatureRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace MerchForge.api.DTOs.Subscriptions
+{
+    public class CreateFeatureRequest
+    {
+        public string Key { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+    }
+}

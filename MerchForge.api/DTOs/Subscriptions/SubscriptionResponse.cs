6c28d8f06fb40365a969a684bd347b60f4e9c987
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-14 14:56:47 +0300

    feat:Created the rest of the DTOs for responses and requests

---
+++ MerchForge.api/DTOs/Subscriptions/SubscriptionResponse.cs
@@ -0,0 +1,20 @@
+﻿using MerchForge.api.Enums;
+
+namespace MerchForge.api.DTOs.Subscriptions;
+
+public class SubscriptionResponse
+{
+    public Guid Id { get; set; }
+
+    public Guid BusinessId { get; set; }
+
+    public Guid PlanId { get; set; }
+
+    public string PlanName { get; set; } = null!;
+
+    public SubscriptionStatus Status { get; set; }
+
+    public DateTime CurrentPeriodStart { get; set; }
+
+    public DateTime CurrentPeriodEnd { get; set; }
+}
---
+++ MerchForge.api/DTOs/Subscriptions/UpdateSubscriptionPlanRequest.cs
@@ -0,0 +1,21 @@
+﻿using MerchForge.api.Enums;
+
+namespace MerchForge.api.DTOs.Subscriptions;
+
+public class UpdateSubscriptionPlanRequest
+{
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string Currency { get; set; } = "USD";
+
+    public BillingInterval BillingInterval { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public List<PlanFeatureRequest> Features { get; set; }
+        = new();
+}

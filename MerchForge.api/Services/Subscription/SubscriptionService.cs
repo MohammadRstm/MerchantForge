72dd384e1e1845cbbf004b910bce21c8277440e8
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-14 16:02:58 +0300

    feat:Implmented subscriptions service

---
+++ MerchForge.api/Services/Subscription/SubscriptionService.cs
@@ -0,0 +1,28 @@
+﻿
+namespace MerchForge.api.Services.Subscription
+{
+    using MerchForge.api.Data;
+    using MerchForge.api.Services.Subscription.interfaces;
+    using MerchForge.api.Models;
+    public class SubscriptionService : ISubscriptionService
+    {
+        private readonly MerchForgeDbContext _db;
+        public SubscriptionService(MerchForgeDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Subscription> HasFeatureAsync(Guid businessId)
+        {
+            return await _db.Subscriptions
+            .Include(s => s.SubscriptionPlan)
+                .ThenInclude(p => p.PlanFeatures)
+                    .ThenInclude(pf => pf.Feature)
+            .Include(s => s.BusinessFeatureOverrides)
+                .ThenInclude(o => o.Feature)
+            .FirstOrDefaultAsync(
+                s => s.BusinessId == businessId);
+        }
+
+    }
+}
---
+++ MerchForge.api/Services/Subscription/interfaces/ISubscriptionService.cs
@@ -0,0 +1,9 @@
+﻿namespace MerchForge.api.Services.Subscription.interfaces
+{
+    using MerchForge.api.Models;
+    public interface ISubscriptionService
+    {
+        Task<Subscription> HasFeatureAsync(Guid businessId);
+
+    }
+}

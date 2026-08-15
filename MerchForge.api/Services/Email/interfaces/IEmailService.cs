55e607e6d7b24715f8f8b6e9c2295f19596f08bf
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-15 16:04:18 +0300

    feat:Created email service interface

---
+++ MerchForge.api/Services/Email/interfaces/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿namespace MerchForge.api.Services.Email.Interfaces;
+
+public interface IEmailService
+{
+    Task SendBusinessOwnerInvitationAsync(
+        string email,
+        string invitationLink,
+        DateTime expiresAt,
+        CancellationToken cancellationToken = default);
+}

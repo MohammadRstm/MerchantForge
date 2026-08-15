4bd40262ab18c84e638d23945a1f102b6bf79604
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-15 15:15:44 +0300

    feat:created invitation response

---
+++ MerchForge.api/DTOs/Invitations/InvitationResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace MerchForge.api.DTOs.Invitations
+{
+    public class InvitationResponse
+    {
+        public Guid Id { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+
+        public DateTime ExpiresAt { get; set; }
+    }
+}

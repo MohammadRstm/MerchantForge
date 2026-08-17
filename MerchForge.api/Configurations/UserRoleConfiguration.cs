8bec8ce19715b8ae4e81d598460d5f7098354363
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-17 17:55:20 +0300

    feat:Added system roles seeder migration

---
+++ MerchForge.api/Configurations/UserRoleConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using MerchForge.api.Enums;
+using MerchForge.api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MerchForge.api.Data.Configurations
+{
+    public class UserRoleConfiguration : IEntityTypeConfiguration<UserRole>
+    {
+        public void Configure(EntityTypeBuilder<UserRole> builder)
+        {
+            builder.HasKey(r => r.Id);
+
+            builder.Property(r => r.Role)
+                .IsRequired()
+                 .HasConversion<string>()
+                .HasMaxLength(50);
+
+            builder.HasData(
+                new UserRole
+                {
+                    Id = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                    Role = SystemRole.SuperAdmin
+                },
+                new UserRole
+                {
+                    Id = Guid.Parse("22222222-2222-2222-2222-222222222222"),
+                    Role = SystemRole.Admin
+                },
+                new UserRole
+                {
+                    Id = Guid.Parse("33333333-3333-3333-3333-333333333333"),
+                    Role = SystemRole.User
+                }
+            );
+        }
+    }
+}
---
+++ MerchForge.api/Migrations/20260817144840_SeedSystemRoles.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MerchForge.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedSystemRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "SystemRoles",
+                columns: new[] { "Id", "Role" },
+                values: new object[,]
+                {
+                    { new Guid("11111111-1111-1111-1111-111111111111"), "SuperAdmin" },
+                    { new Guid("22222222-2222-2222-2222-222222222222"), "Admin" },
+                    { new Guid("33333333-3333-3333-3333-333333333333"), "User" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "SystemRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"));
+
+            migrationBuilder.DeleteData(
+                table: "SystemRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"));
+
+            migrationBuilder.DeleteData(
+                table: "SystemRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"));
+        }
+    }
+}

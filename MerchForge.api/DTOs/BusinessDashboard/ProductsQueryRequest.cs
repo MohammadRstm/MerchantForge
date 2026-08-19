57e1629ae18e6ac9b847596b289da37e3d4b945f
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-19 08:17:01 +0300

    feat: add ProductsQueryRequest DTO for the business products table

---
+++ MerchForge.api/DTOs/BusinessDashboard/ProductsQueryRequest.cs
@@ -0,0 +1,15 @@
+using MerchForge.api.DTOs.Common;
+using MerchForge.api.Enums;
+
+namespace MerchForge.api.DTOs.BusinessDashboard;
+
+public class ProductsQueryRequest : PagedQuery
+{
+    public string? Search { get; set; }
+
+    public string? Category { get; set; }
+
+    public ProductSortField SortBy { get; set; } = ProductSortField.CreatedAt;
+
+    public bool SortDescending { get; set; } = true;
+}

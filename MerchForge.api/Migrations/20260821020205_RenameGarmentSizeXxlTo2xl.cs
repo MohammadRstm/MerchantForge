d44b3aa851937b2b4d89b03366de4dd12c93466c
Mohammad Rostom <mohammadrstm2004@gmail.com>
2026-08-21 05:02:30 +0300

    fix: use 2XL instead of XXL in the fashion sizes allowlist
    2XL is the label real garment listings (and the fashion storefront
    template) actually use; XXL was never referenced by any seeded data.

---
+++ MerchForge.api/Migrations/20260821020205_RenameGarmentSizeXxlTo2xl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MerchForge.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameGarmentSizeXxlTo2xl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "product_attribute_definitions",
+                keyColumn: "Id",
+                keyValue: new Guid("a1000000-0000-4000-8000-000000000002"),
+                column: "AllowedValues",
+                value: "[\"XS\",\"S\",\"M\",\"L\",\"XL\",\"2XL\"]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "product_attribute_definitions",
+                keyColumn: "Id",
+                keyValue: new Guid("a1000000-0000-4000-8000-000000000002"),
+                column: "AllowedValues",
+                value: "[\"XS\",\"S\",\"M\",\"L\",\"XL\",\"XXL\"]");
+        }
+    }
+}
